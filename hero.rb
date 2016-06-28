247d2a1baf4d6e9fe8cb91b21600b2ff95326ddb
Lozitskiy Vladimir <loza-w@mail.ru>
2016-06-28 17:37:36 +0300

    Add hero entity

--- hero.rb
+++ hero.rb
@@ -1,3 +1,29 @@
 class Hero
+  # constructor get name,health
+def initialize(name,health=100)
+  @name = name.capitalize
+  @health = health
+end
+#becose i can display @name
+def name
+   @name
+end
+# decrease health on 10
+def power_down
+  @health -= 10
+end
 
-end  
+def power_up
+@health += 10
+end
+
+def hero_info
+  puts "#{@name} has a  health of #{@health}"
+end
+end
+
+hero = Hero.new 'mike'
+puts hero.name
+puts hero.power_up
+puts hero.power_down
+puts hero.hero_info
